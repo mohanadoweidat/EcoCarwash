2f6cc06af3dbd641a7625117fd30b2bf5b2ce46c
Mohanad <33122095+mohanadoweidat@users.noreply.github.com>
2022-09-20 13:58:55 +0200

    Add test page.

---
+++ EcoCarwash/index.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="EcoCarwash.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btn" runat="server" Text="Button" />
+            <input id="htmlBtn" type="button" value="button" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ EcoCarwash/index.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EcoCarwash
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            htmlBtn.ServerClick += HtmlBtn_ServerClick;
+            btn.Click += Btn_Click;
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HtmlBtn_ServerClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
